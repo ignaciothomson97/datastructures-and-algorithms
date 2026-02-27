04603dfdd931d1b731941651ea0b1df6c34dba23
Ignacio Thomson <ignacio.thomson.97@gmail.com>
2026-02-26 22:36:19 -0300

    Added TwoSum with O(N^2) and O(N) solutions.

--- app/src/main/java/cl/datastructuresandalgorithms/Main.java
+++ app/src/main/java/cl/datastructuresandalgorithms/Main.java
@@ -1,11 +1,13 @@
 package cl.datastructuresandalgorithms;
 
 import cl.datastructuresandalgorithms.containsduplicate.ContainsDuplicate;
+import cl.datastructuresandalgorithms.twosum.TwoSum;
 import cl.datastructuresandalgorithms.validanagram.ValidAnagram;
 
 public class Main {
     public static void main(String[] args) {
         //ContainsDuplicate.containsDuplicate();
-        ValidAnagram.validAnagram("bat", "tabi");
+        //ValidAnagram.validAnagram("bat", "tabi");
+        TwoSum.twoSum(new int[]{2, 7, 11, 15}, 9);
     }
 }
---
+++ app/src/main/java/cl/datastructuresandalgorithms/twosum/TwoSum.java
@@ -0,0 +1,38 @@
+package cl.datastructuresandalgorithms.twosum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class TwoSum {
+
+    public static void twoSum(int[] nums, int target) {
+
+        // Enfoque de  brute force, O(N^2)
+        int[] indexes = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    indexes[0] = i;
+                    indexes[1] = j;
+                }
+            }
+        }
+        System.out.println("Indexes: " + Arrays.toString(indexes));
+
+        // Esta es la solución más óptima O(N)
+        HashMap<Integer, Integer> seen = new HashMap<>();
+        int[] indexes2 = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int remainder = target - nums[i];
+            if (seen.containsKey(remainder)) {
+                indexes2[0] = seen.get(remainder);
+                indexes2[1] = i;
+            }
+            seen.put(nums[i], i);
+        }
+        System.out.println("Indexes 2: " + indexes2);
+    }
+
+}
